451338880582130aa81f79733224f1d367284411
McCune1224 <alexmccune1224@gmail.com>
2022-11-11 23:36:40 -0800

    App will now either render login page or dashboard if access token cookie is set. Added Spotify Web API for future use

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,27 @@
+import { useCookies } from "react-cookie"
 import Login from "./components/Login"
+import Dashboard from "./components/Dashboard"
 
 function App() {
 
+    const [cookies, setCookies] = useCookies<string>(["accessToken"])
+
+
+    if (!cookies["AccessToken"]) {
+        return (
+            <div className="bg-zinc-900 px-4 sm:px-6 md:px-12 max-w-3xl mx-auto w-screen">
+                <Login />
+            </div>
+        )
+    }
     return (
         <div className="bg-zinc-900 px-4 sm:px-6 md:px-12 max-w-3xl mx-auto w-screen">
-            <Login />
-
+            <Dashboard token={cookies["AccessToken"]} />
         </div>
+
     )
+
+
 }
 
 export default App
--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import 'spotify-web-api-js'
+import SpotifyWebApi from 'spotify-web-api-js'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
+    const spoAPI = new SpotifyWebApi()
+    spoAPI.setAccessToken(props.token)
+
+
+    const [user, setUser] = useState<SpotifyApi.CurrentUsersProfileResponse>()
+
+    useEffect(() => {
+        spoAPI.getMe().then((spotifyResponse) => setUser(spotifyResponse))
+    }, [])
+
+
+
+
     return (
-        <div>Dashboard</div>
+        <div className="text-white"><p>Hello {user?.display_name}!</p></div>
     )
 }
 
