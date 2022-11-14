ec6d96f10a64037799997bbc57f08418b4f2275d
McCune1224 <alexmccune1224@gmail.com>
2022-11-13 18:58:55 -0800

    trimmed artists to only be top 10, and added background for temporary display

--- frontend/src/components/Artists.tsx
+++ frontend/src/components/Artists.tsx
@@ -15,7 +15,14 @@ const Artists = (props: SpotifyClient) => {
 
     return (
         <div>
-            {artists && artists.items.map((artist) => <p>{artist.name}</p>)}
+            {artists && artists.items.slice(0, 10).map((artist, i) =>
+                <div className="flex bg-slate-200" key={artist.id}>
+                    <img className="w-20 h-20 rounded-full" loading="lazy" src={artist.images[0].url} />
+                    <p>{i + 1}</p>
+                    <p >{artist.name}</p>
+                    <p >{artist.genres[0]}</p>
+                </div>
+            )}
         </div>
     )
 }
